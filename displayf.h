8c0e390480fa1b9de2fadc0215e31edba30bfcda
broketya352-dev <broketya352@gmail.com>
2026-06-13 10:36:35 +0700

    Update displayf.h

---
+++ displayf.h
@@ -0,0 +1,10 @@
+#ifndef DISPLAYFLOWER_H
+#define DISPLAYFLOWER_H
+
+#include "data.h"
+void inatialflower(Flower *flowers, int *count);
+
+void displayFlower(Flower *flowers,
+                   int count);
+
+#endif
